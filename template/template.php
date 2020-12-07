8b2781f5b75853736f4c1871dbf90e98641a74d9
okada-bruteforce <kd1284895@st.kobedenshi.ac.jp>
2020-12-07 13:54:36 +0900

    [Add] template

--- template/template.php
+++ template/template.php
@@ -3,64 +3,115 @@ class template {
 
     public function header($title)
     {
-        echo htmlspecialchars('<!DOCTYPE html>'). "<br>";
-        echo htmlspecialchars('<html lang="ja">'). "<br>";
-        echo htmlspecialchars('<head>'). "<br>";
-        echo htmlspecialchars('<meta charset="UTF-8">'). "<br>";
-        echo htmlspecialchars('<title>'. $title. '</title>'). "<br>";
-        echo htmlspecialchars('</head>'). "<br>";
+        $tmp = '<!DOCTYPE html>'. "\n";
+        $tmp .= '<html lang="ja">'. "\n";
+        $tmp .= '<head>'. "\n";
+        $tmp .= '<meta charset="UTF-8">'. "\n";
+        $tmp .= '<title>'. $title. '</title>'. "\n";
+        $tmp .= '</head>'. "\n";
+        return $tmp;
     }
     
     public function div($contents, $class, $id)
     {
-        echo htmlspecialchars('<div');
+        $tmp = '<div';
         
         if (!empty($class)) {
-            echo " class=". $class;
+            $tmp .= " class=". $class;
         } elseif (!empty($id)) {
-            echo " id=". $id;
+            $tmp .= " id=". $id;
         }
         
-        echo htmlspecialchars('>'). "<br>";
-        echo htmlspecialchars($contents). "<br>";
-        echo htmlspecialchars('</div>'). "<br>";
+        $tmp .= '>'. "\n";
+        $tmp .= $contents. "\n";
+        $tmp .= '</div>'. "\n";
+        return $tmp;
     }
     
     public function span($contents, $class, $id)
     {
-        echo htmlspecialchars('<span');
+        $tmp = '<span';
         
         if (!empty($class)) {
-            echo " class=". $class;
+            $tmp .= " class=". $class;
         } elseif (!empty($id)) {
-            echo " id=". $id;
+            $tmp .= " id=". $id;
         }
         
-        echo htmlspecialchars('>'). "<br>";
-        echo htmlspecialchars($contents). "<br>";
-        echo htmlspecialchars('</span>'). "<br>";
+        $tmp .= '>'. "\n";
+        $tmp .= $contents. "\n";
+        $tmp .= '</span>'. "\n";
+        return $tmp;
     }
 
     public function h1_6($contents, $size)
     {
-        echo htmlspecialchars('<h'. $size. '>'). "<br>";
-        echo htmlspecialchars($contents). "<br>";
-        echo htmlspecialchars('</h'. $size. '>'). "<br>";
+        $tmp = '<h'. $size. '>'. "\n";
+        $tmp .= $contents. "\n";
+        $tmp .= '</h'. $size. '>'. "\n";
+        return $tmp;
     }
     
     public function p($contents)
     {
-        echo htmlspecialchars('<p>'. $contents. '</p>'). "<br>";
+        $tmp = '<p>'. $contents. '</p>'. "\n";
+        return $tmp;
     }
     
     public function br()
     {
-        echo htmlspecialchars('<br>'). "<br>";
+        $tmp = '<br>'. "\n";
+        return $tmp;
     }
 
     public function a($link, $contents)
     {
-        echo htmlspecialchars('<a href="'. $link. '">'. $contents. '</a>'). "<br>";
+        $tmp = '<a href="'. $link. '">'. $contents. '</a>'. "\n";
+        return $tmp;
+    }
+
+    public function link($link, $contents)
+    {
+        $tmp = '<link rel="'. $contents. '" href="'. $link. '">'. "\n";
+        return $tmp;
+    }
+
+    public function img($link, $contents)
+    {
+        $tmp = '<img src="'. $link. '" alt="'. $contents. '">'. "\n";
+        return $tmp;
+    }
+
+    public function table($contents)
+    {
+        $tmp = "<table>\n";
+        $tmp .= $contents. "\n";
+        $tmp .= "</table>\n";
+        return $tmp;
+    }
+
+    public function th($contents)
+    {
+        $tmp = "<th>\n";
+        $tmp .= $contents. "\n";
+        $tmp .= "</th>\n";
+        return $tmp;
+    }
+
+    public function tr($contents)
+    {
+        $tmp = "<tr>\n";
+        $tmp .= $contents. "\n";
+        $tmp .= "</tr>\n";
+        return $tmp;
+    }
+
+    public function td($contents)
+    {
+        $tmp = "<td>\n";
+        $tmp .= $contents. "\n";
+        $tmp .= "</td>\n";
+        return $tmp;
     }
 }
 ?>
--- template/test.php
+++ template/test.php
@@ -12,12 +12,16 @@ $title = "タイトル";
 $link = "index.html";
 // --test
 
-$template->header($title);
-$template->div($contents, $class, $id);
-$template->span($contents, $class, $id);
-$template->h1_6($contents, $size);
-$template->p($contents);
-$template->br();
-$template->a($link, $contents);
+echo nl2br(htmlspecialchars($template->header($title)));
+echo nl2br(htmlspecialchars($template->div($template->h1_6($contents, $size), $class, $id)));
+echo nl2br(htmlspecialchars($template->span($contents, $class, $id)));
+echo nl2br(htmlspecialchars($template->h1_6($contents, $size)));
+echo nl2br(htmlspecialchars($template->p($contents)));
+echo nl2br(htmlspecialchars($template->br()));
+echo nl2br(htmlspecialchars($template->a($link, $contents)));
+echo nl2br(htmlspecialchars($template->link($link, $contents)));
+echo nl2br(htmlspecialchars($template->img($link, $contents)));
+echo nl2br(htmlspecialchars($template->table($contents)));
+echo nl2br(htmlspecialchars($template->td($template->tr($contents))));
 
 ?>
