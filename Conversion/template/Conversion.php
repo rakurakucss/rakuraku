7f7825f4b68697d6af262ca217a61807e2d3f542
okada-bruteforce <59984901+okada-bruteforce@users.noreply.github.com>
2021-01-18 14:25:07 +0900

    Update Conversion.php

--- Conversion/template/Conversion.php
+++ Conversion/template/Conversion.php
@@ -2,25 +2,18 @@
 require_once __DIR__. '/template.php';
 $template = new template();
 
-// test--
-
     $convary = $_POST['ary'];
-//   if(isset($_POST['ary'])){
-//     // POSTで送られてきた場合
-//     $convary = $_POST['ary'];
-//   }else{
-//     // GETで送られてきた場合
-//     echo "not get ary";
-//   }
 
 Conversion($template, $convary);
-// --test
 
 function Conversion($template, $convary)
 {
     foreach ($convary as $array) {
 
         switch ($array[1]) {
+            case "0":
+            break;
+            
             case "3":
                 echo nl2br(htmlspecialchars($template->div($array[2], $array[3], $array[5])));
             break;
